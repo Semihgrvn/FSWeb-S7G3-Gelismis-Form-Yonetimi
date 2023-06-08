7196299bdcd77839f90601909630cada928f97f9
Semih Gervan <semihgervan@hotmail.com>
2023-06-08 14:55:12 +0300

    S7G3 Projesi

---
+++ kullanici-katilimi/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyForm from './Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
